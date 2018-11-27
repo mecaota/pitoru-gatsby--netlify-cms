cd8e00e5fed6f95b4917f811ff40e5542e491bb0
mecaota <b1014167@fun.ac.jp>
2018-11-27 23:09:27 +0900

    navbarをモバイル版で開閉対応化、他一部コンテンツ修正

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,43 +2,65 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faGithubSquare
-} from '@fortawesome/free-brands-svg-icons';
+} from '@fortawesome/free-brands-svg-icons'
 import { Link } from 'gatsby'
 import logo from '../img/logo.png'
 
-const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="多分ムーミン"/>
-          </figure>
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/blog">
-          Blog
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Works
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/mecaota/pitoru-gatsby--netlify-cms"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <FontAwesomeIcon icon={faGithubSquare} size="2x"/>
-        </a>
-      </div>
-    </div>
-  </nav>
-)
-
-export default Navbar
+export default class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggle: false // toggleの初期状態を設定
+    };
+  }
+  func(e) {
+    this.setState({
+      toggle: !this.state.toggle // state.toggleフラグを反転させる
+    });
+  }
+  render() {
+    // state.toggleを見てonを結合するか否かを決める
+    return (
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item">
+              <figure className="image">
+                <img src={logo} alt="多分ムーミン" width="28" height="28"/>
+              </figure>
+            </Link>
+            <Link to="#" role="button" className={this.state.toggle ? "navbar-burger is-active" : "navbar-burger"} onClick={this.func.bind(this)} data-target="navMenu" aria-label="menu" aria-expanded="false">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </Link>
+          </div>
+          <div id="navMenu" className={this.state.toggle ? "navbar-menu is-active" : "navbar-menu"}>
+            <div className="navbar-start">
+              <Link className="navbar-item" to="/about">
+                About
+              </Link>
+              <Link className="navbar-item" to="/blog">
+                Blog
+              </Link>
+              <Link className="navbar-item" to="/products">
+                Works
+              </Link>
+            </div>
+            <div className="navbar-end">
+              <a
+                className="navbar-item"
+                href="https://github.com/mecaota/pitoru-gatsby--netlify-cms"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+              <FontAwesomeIcon icon={faGithubSquare} size="2x"/>
+              </a>
+            </div>
+          </div>
+    
+        </div>
+      </nav>
+    )
+  }
+}
